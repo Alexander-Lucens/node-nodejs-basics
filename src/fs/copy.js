5b43462b4e4fd42d0730fad2f5b0c39bf66aa27e
Alexander Kuzmin <kuzminalex96@icloud.com>
2025-10-20 20:52:06 +0200

    Just starts course, and i dont realy expects that ther will be so much excercises. But whatever. Goal: 21.10 - 2-3 hours finish fs, cli, modules, 22.10 - hash, streams, zip, 23.10 - wt, cp

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-  // Write your code here
+  
+  const oldDir = path.join(__dirname, 'files');
+  const newDir = path.join(__dirname, 'files_copy');
+
+  try {
+
+    await fs.access(oldDir);
+
+    try {
+      await fs.access(newDir);
+      throw new Error('FS operation failed');
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw new Error('FS operation failed');
+      }
+    }
+
+    await fs.cp(oldDir, newDir, { recursive: true });
+    
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-  // Write your code here
+  /* 
+    Safity feature.
+    Allow to call `create.js` from warious places and have same resule.
+  */
+  const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
+  try {
+    await fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx'});
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
